cd3023321b3dcba76d903357d028a8ba1a9fe20f
Vasu <vasudevanmech7603@gmail.com>
2026-08-15 20:35:19 +0530

    first commit

--- components/sub/skill-data-provider.tsx
+++ components/sub/skill-data-provider.tsx
@@ -2,7 +2,6 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { useInView } from "react-intersection-observer";
 
 type SkillDataProviderProps = {
   src: string;
@@ -19,14 +18,12 @@ export const SkillDataProvider = ({
   height,
   index,
 }: SkillDataProviderProps) => {
-  const { ref, inView } = useInView({ triggerOnce: true });
-
   return (
     <motion.div
-      ref={ref}
-      initial={{ opacity: 0 }}
-      animate={inView ? { opacity: 1 } : {}}
-      transition={{ delay: index * 0.07, duration: 0.4 }}
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ delay: index * 0.1, duration: 0.5 }}
     >
       <Image
         src={`/skills/${src}`}
